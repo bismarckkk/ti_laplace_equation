2718e68f13ca048b0bc43477edae145f00a2771f
左清宇 <zuoqingyu@nuaa.edu.cn>
2022-08-19 13:43:09 +0800

    [feat] 填充新点时边界速度改为并行计算// [misc] 将部分magicNumber改为变量

--- demo.py
+++ demo.py
@@ -4,6 +4,9 @@ ti.init(arch=ti.cpu, cpu_max_num_threads=1)
 
 screen = (30, 20)
 meshSpace = 20
+maxItems = 20
+refillFrame = 20
+refillVelThresh = .1
 gui = ti.GUI('demo', tuple(it*meshSpace for it in screen))
 guiHeight = meshSpace * screen[1]
 vec2 = ti.types.vector(2, float)
@@ -11,18 +14,19 @@ V = vec2(1., 0)
 dt = .002
 
 points = ti.Vector.field(2, float, screen[0] * screen[1] * 2)
+boundaryVel = ti.Vector.field(2, float, (4, max(*screen)))
 sources = ti.Struct.field({
     "pos": vec2,
     "q": ti.f32
-}, shape=20)
+}, shape=maxItems)
 vortexes = ti.Struct.field({
     "pos": vec2,
     "q": ti.f32
-}, shape=20)
+}, shape=maxItems)
 dipoles = ti.Struct.field({
     "pos": vec2,
     "m": ti.f32
-}, shape=20)
+}, shape=maxItems)
 start = ti.field(ti.i32, shape=2)
 lastStart = ti.field(ti.i32, shape=2)
 points.fill(-100)
@@ -42,16 +46,16 @@ def initPoints():
 @ti.func
 def getVel(pos):
     vel = V
-    for i in range(20):
+    for i in range(maxItems):
         uv = pos - sources[i].pos
         uv[0] *= screen[1] / screen[0]
         vel += uv * sources[i].q / (2 * ti.math.pi * (uv[0] ** 2 + uv[1] ** 2))
-    for i in range(20):
+
         uv = pos - vortexes[i].pos
         uv = vec2(-uv[1], uv[0])
         uv[0] *= screen[1] / screen[0]
         vel += uv * vortexes[i].q / (2 * ti.math.pi * (uv[0] ** 2 + uv[1] ** 2))
-    for i in range(20):
+
         uv = pos - dipoles[i].pos
         uv[0] *= screen[1] / screen[0]
         vel_t = vec2(uv[1]**2 - uv[0]**2, -2*uv[0]*uv[1])
@@ -82,6 +86,31 @@ def updateStart():
 
 
 @ti.kernel
+def updateBoundaryVel():
+    for i, j in ti.ndrange(4, ti.max(*screen)):
+        if i == 0:
+            if j >= screen[1]:
+                continue
+            pos = vec2(0, (j + .5) / screen[1])
+            boundaryVel[i, j] = getVel(pos)
+        elif i == 1:
+            if j >= screen[0]:
+                continue
+            pos = vec2((j + .5) / screen[0], 0)
+            boundaryVel[i, j] = getVel(pos)
+        elif i == 2:
+            if j >= screen[1]:
+                continue
+            pos = vec2(1, (j + .5) / screen[1])
+            boundaryVel[i, j] = getVel(pos)
+        elif i == 3:
+            if j >= screen[0]:
+                continue
+            pos = vec2((j + .5) / screen[0], 1)
+            boundaryVel[i, j] = getVel(pos)
+
+
+@ti.kernel
 def refillPoints():
     ti.loop_config(serialize=True)
     started = False
@@ -93,28 +122,20 @@ def refillPoints():
                 else:
                     started = True
                 if start[0] == 0:
-                    pos = vec2(0, (start[1] + .5) / screen[1])
-                    vel = getVel(pos)
-                    if vel[0] > .1:
-                        points[i] = pos
+                    if boundaryVel[0, start[1]][0] > refillVelThresh:
+                        points[i] = vec2(0, (start[1] + .5) / screen[1])
                         break
                 elif start[0] == 1:
-                    pos = vec2((start[1] + .5) / screen[0], 0)
-                    vel = getVel(pos)
-                    if vel[1] > .1:
-                        points[i] = pos
+                    if boundaryVel[1, start[1]][1] > refillVelThresh:
+                        points[i] = vec2((start[1] + .5) / screen[0], 0)
                         break
                 elif start[0] == 2:
-                    pos = vec2(1, (start[1] + .5) / screen[1])
-                    vel = getVel(pos)
-                    if vel[0] < -.1:
-                        points[i] = pos
+                    if boundaryVel[2, start[1]][0] < -refillVelThresh:
+                        points[i] = vec2(1, (start[1] + .5) / screen[1])
                         break
                 elif start[0] == 3:
-                    pos = vec2((start[1] + .5) / screen[0], 1)
-                    vel = getVel(pos)
-                    if vel[1] < -.1:
-                        points[i] = pos
+                    if boundaryVel[3, start[1]][1] < -refillVelThresh:
+                        points[i] = vec2((start[1] + .5) / screen[0], 1)
                         break
     lastStart[0] = start[0]
     lastStart[1] = start[1]
@@ -129,7 +150,7 @@ def updatePoints():
             points[i] += vel * dt
         else:
             points[i] = vec2(-100, -100)
-        for j in range(20):
+        for j in range(maxItems):
             if sources[j].q < 0 and ti.math.length(points[i] - sources[j].pos) < 0.025:
                 points[i] = vec2(-100, -100)
             if dipoles[j].m != 0 and ti.math.length(points[i] - dipoles[j].pos) < 0.05:
@@ -146,7 +167,7 @@ def drawMark(gui, frame):
         vec2(1 * screen[1] / screen[0], 1) / (guiHeight),
         vec2(-1 * screen[1] / screen[0], -1) / (guiHeight),
     ]
-    for i in range(20):
+    for i in range(maxItems):
         if dipoles[i].m > 0:
             gui.circle(dipoles[i].pos, 0xFFFDC0, dipoles[i].m * 2000)
         elif dipoles[i].m < 0:
@@ -182,7 +203,7 @@ def drawMark(gui, frame):
 def processGuiEvent(gui):
     while gui.get_event((ti.GUI.PRESS, ti.GUI.LMB), (ti.GUI.PRESS, ti.GUI.RMB)):
         if gui.is_pressed(ti.GUI.LMB) and gui.is_pressed(ti.GUI.RMB):
-            for i in range(20):
+            for i in range(maxItems):
                 if sources[i].q != 0 and (sources[i].pos - vec2(*gui.get_cursor_pos())).norm() < 5 / guiHeight:
                     sources[i].q = 0
                 if vortexes[i].q != 0 and (vortexes[i].pos - vec2(*gui.get_cursor_pos())).norm() < 5 / guiHeight:
@@ -191,7 +212,7 @@ def processGuiEvent(gui):
                     dipoles[i].m = 0
         else:
             if gui.is_pressed('s'):
-                for i in range(20):
+                for i in range(maxItems):
                     if sources[i].q == 0:
                         if gui.is_pressed(ti.GUI.RMB):
                             sources[i].q -= 1
@@ -200,7 +221,7 @@ def processGuiEvent(gui):
                         sources[i].pos = vec2(*gui.get_cursor_pos())
                         break
             elif gui.is_pressed('v'):
-                for i in range(20):
+                for i in range(maxItems):
                     if vortexes[i].q == 0:
                         if gui.is_pressed(ti.GUI.RMB):
                             vortexes[i].q -= 0.5
@@ -209,7 +230,7 @@ def processGuiEvent(gui):
                         vortexes[i].pos = vec2(*gui.get_cursor_pos())
                         break
             elif gui.is_pressed('d'):
-                for i in range(20):
+                for i in range(maxItems):
                     if dipoles[i].m == 0:
                         if gui.is_pressed(ti.GUI.RMB):
                             dipoles[i].m -= 0.01
@@ -218,7 +239,7 @@ def processGuiEvent(gui):
                         dipoles[i].pos = vec2(*gui.get_cursor_pos())
                         break
             else:
-                for i in range(20):
+                for i in range(maxItems):
                     if sources[i].q != 0 and (sources[i].pos - vec2(*gui.get_cursor_pos())).norm() < 5 / guiHeight:
                         if gui.is_pressed(ti.GUI.RMB):
                             sources[i].q -= 0.5 * int(sources[i].q >= 0.0) - (sources[i].q <= 0.0)
@@ -238,6 +259,7 @@ def processGuiEvent(gui):
 
 if __name__ == '__main__':
     initPoints()
+    updateBoundaryVel()
     refillPoints()
     refillCount = 0
     frame = 0
@@ -246,8 +268,9 @@ if __name__ == '__main__':
         updatePoints()
         ps = points.to_numpy()
         gui.circles(ps, 3, color=0x2E94B9)
-        if refillCount > 20:
+        if refillCount > refillFrame:
             refillCount = 0
+            updateBoundaryVel()
             refillPoints()
         drawMark(gui, frame)
         # gui.show(f'./frames/frame_{frame}.png')
